591bc878cd9326cb060bf2b7ce565ff68698a6d3
Nathan Ryan <rockymountainshredder@gmail.com>
2024-07-04 00:06:09 -0600

    Created inital Vue 3 Expense Tracker using Composition API and local storage

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,94 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
-    <IncomeExpenses />
-    <TransactionList />
-    <AddTransaction />
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted"/>
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted"/>
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from './components/Header.vue';   
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction,
-  },
+import { useToast } from 'vue-toastification';
+
+import { ref, computed, onMounted } from 'vue';
+
+const toast = useToast();
+
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+  if(savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+
+});
+
+// Get Total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+// Get income
+const income = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount > 0)
+  .reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0)
+  .toFixed(2);
+});
+
+// Get expenses
+const expenses = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount < 0)
+  .reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0)
+  .toFixed(2);
+});
+
+// Add Transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  });
+
+  savedTransactionsToLocalStorage();
+
+  toast.success('Transaction added');
 };
+
+// Generate Unique Id
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+}
+
+// Delete Transaction
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction) => transaction.id !== id);
+
+    savedTransactionsToLocalStorage();
+
+    toast.success('Transaction deleted');
+};
+
+// Save to localStorage
+const savedTransactionsToLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value));
+}
 </script>
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -20,3 +20,32 @@
         <button class="btn">Add transaction</button>
     </form>
 </template>
+
+<script setup>
+import {ref} from 'vue';
+import {useToast} from 'vue-toastification';
+
+const text = ref('');
+const amount = ref('');
+
+const emit = defineEmits(['transactionSubmitted']);
+
+const toast = useToast();
+
+const onSubmit = () => {
+    if (!text.value || !amount.value) {
+        toast.error('Both fields must be filled');
+        return;
+    }
+
+    const transactionData = {
+        text: text.value,
+        amount: parseFloat(amount.value)
+    }
+
+    emit('transactionSubmitted', transactionData);
+
+    text.value = '';
+    amount.value = '';
+};
+</script>
